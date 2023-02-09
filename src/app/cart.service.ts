c756288239b19088f78bdbbd12d1230f95093b4d
Vito02 <91742997+Vito02@users.noreply.github.com>
2023-02-09 16:42:30 +0100

    Use the cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -8,4 +8,18 @@ export class CartService {
   constructor() {}
 
   items: Product[] = [];
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+  
 }
